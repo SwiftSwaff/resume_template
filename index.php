4780466860b5ab4ed267306010c9a8bc8337de42
SwiftSwaff <jeff.geyer.29@gmail.com>
2021-12-26 21:53:54 -0800

    Added index.php for bringing components together to form the resume

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Resume</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <div class="resume-container">
+            <?php include_once("modules/header.php"); ?>
+            <?php include_once("modules/resume.php"); ?>
+            <?php include_once("modules/footer.php"); ?>
+        </div>
+    </body>
+</html>
