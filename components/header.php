a0904c11758b4b4b2fb9dca3a3673f9b5ba53754
SwiftSwaff <jeff.geyer.29@gmail.com>
2021-12-26 21:52:21 -0800

    Initial commit

---
+++ components/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Jeff Geyer</h1>
+    <ul class="personal-info">
+        <li><img src="/resume/resources/mail-icon.png"><span>jeff.geyer.29@gmail.com</span></li>
+        <li><img src="/resume/resources/phone-icon.png"><span>(778) 809-4647</span></li>
+        <li><img src="/resume/resources/linkedin-icon.png"><span>https://www.linkedin.com/in/jeff-geyer/</span></li>
+        <li><img src="/resume/resources/location-icon.png"><span>1526 E 10th Avenue<br/>Vancouver, BC, V5N 1X5</span></li>
+    </ul>
+</header>
---
+++ components/resume.php
@@ -0,0 +1,80 @@
+<main>
+    <section id='timeline'>
+        <div class='timeline-border'></div>
+        <div class='timeline-block'>
+            <svg class='timeline-icon header' width='25' height='25'>
+                <rect x="10" y="-7" width="15" height="15" transform='rotate(45)'/>
+            </svg>
+            <h2>Career Highlights</h2>
+            <div class='timeline-content'>
+                <ul class='timeline-list'>
+                    <li>3 Years in the Industry.</li>
+                    <li>Spearheaded a massive refactoring project of several thousand lines of code, alleviating maintainability concerns and dramatically reducing ticket load times by over 50%.</li>
+                    <li>Authored complex batch ticket processor for bulk actioning by support staff, eliminating many manual ticket lookups and enabling automatic data entry on hundreds of tickets at a time.</li>
+                </ul>
+            </div>
+        </div>
+        <div class='timeline-block'>
+            <svg class='timeline-icon header' width='25' height='25'>
+                <rect x="10" y="-7" width="15" height="15" transform='rotate(45)'/>
+            </svg>
+            <h2>Technical Skills</h2>
+            <div class='timeline-content'>
+                <ul class='timeline-list'>
+                    <li>Languages: PHP, HTML5, CSS (SASS), JavaScript (JQuery, Ajax), MySQL</li>
+                    <li>Additional Skills: Github, RESTful API's, Responsive Design, MVC, CMS, WordPress, Scrum</li>
+                </ul>
+            </div>
+        </div>
+        <div class='timeline-block'>
+            <svg class='timeline-icon header' width='25' height='25'>
+                <rect x="10" y="-7" width="15" height="15" transform='rotate(45)'/>
+            </svg>
+            <h2 style='margin-bottom: 10px;'>Relevant Experience</h2>
+            <div class='timeline-content'>
+                <div class='timeline-content'>
+                    <h3>Software Developer - Freelancer [2019 - Present]</h3>
+                    <ul class='timeline-list'>
+                        <li>Prototyped a responsive, lightweight CMS for a senior sports association to use as a substitute for WordPress, which scores 90+ in all categories on Google Lighthouse.</li>
+                    </ul>
+                </div>
+                <div class='timeline-content'>
+                    <h3>Software Developer - Eigen Development [2016 - 2018]</h3>
+                    <ul class='timeline-list'>
+                        <li>Initiated massive refactoring project to incorporate object-oriented design into the tool suite, improving maintainability of the codebase and reducing ticket load times by over 50%.</li>
+                        <li>Collaborated with support to identify the filters for a batch ticket processing system to eliminate many manual ticket lookups and automatically enter/update data.</li>
+                        <li>Maintained and iterated on company-wide tool suite, building on existing PHP and JavaScript to add over a dozen new tools over time.</li>
+                        <li>Created product tracking front-end to help support staff discover whereabouts of orphaned devices across all customers.</li>
+                        <li>Prepared a set of scheduled nightly/weekly scripts to automate internal tasks, improving efficiency and freeing up roughly 30-60 minutes a week for support staff.</li>
+                        <li>Communicated regularly with managers and staff to determine requirements for updates to the tool suite to ensure that changes would meet expectations fully.</li>
+                        <li>Participated in weekly scrum meetings with the team to keep everyone focused and synchronized.</li>
+                    </ul>
+                </div>
+                <div class='timeline-content'>
+                    <h3>Software Developer - Freelancer [2015 - 2016]</h3>
+                    <ul class='timeline-list'>
+                        <li>Worked with a designer to produce custom-catered WordPress themes, ranging from wedding portals to company portfolios.</li>
+                        <li>Implemented WordPress theme and features for an older version of the Ferrari & Maserati of Vancouver website.</li>
+                    </ul>
+                </div>
+                <div class='timeline-content'>
+                    <h3>Junior Software Developer (Co-op) - CopperTree Analytics [2013]</h3>
+                    <ul class='timeline-list'>
+                        <li>Developed back-end UI for field technicians to interface with on sites at various schools.</li>
+                        <li>Introduced MVC framework template using Python to serve for future back-end UI projects.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class='timeline-block'>
+            <svg class='timeline-icon header' width='25' height='25'>
+                <rect x="10" y="-7" width="15" height="15" transform='rotate(45)'/>
+            </svg>
+            <h2>Education</h2>
+            <div class='timeline-content'>
+                <h3>Bachelor of Science: Computer Science [2014]</h3>
+                <span style='font-style: italic;'>Simon Fraser University - Burnaby, BC</span>
+            </div>
+        </div>
+    </section>
+</main>
